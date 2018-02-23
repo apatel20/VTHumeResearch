ad6ec4cbdf70f872108ce108109bd7a9e3c0133d
apatel20 <apatel20@vt.edu>
2018-02-23 15:08:43 -0500

    added comments and begin training code

--- cnn.py
+++ cnn.py
@@ -14,13 +14,15 @@ import numpy as np
 #loading the dataset with both training and testing
 
 with open(dataFile_training, mode = 'rb') as file:
-	training_data = pickle.load(file)
+	training_data = pickle.load(file)#reads from the open file stream after loading the training data file
 
-with open(dataFile_testing, mode - 'rb') as file:
-	testing_data = pickle.load(file)
+with open(dataFile_testing, mode = 'rb') as file:
+	testing_data = pickle.load(file)#reads from file stream object used for testing against neural network
 	
-X_training_data, y_training_data = training_data['features'], training_data['labels']
+X_training_data, y_training_data = training_data['features'], training_data['labels']#loading datasets to memory 
 X_testing_data, y_testing_data = testing_data['features'], testing_data['labels']
+#features is an array which contains the pixel data of the images
+#labels contains information about the traffic sign such as the picture ID
 
 #finished loading dataset
 
@@ -32,3 +34,14 @@ def grayScale(image_data):
 	
 X_training_data = np.array([grayScale(image_data) for image_data in X_training_data]) #grayscales images in training data
 X_testing_data = np.array([grayScale(image_data for image_data in X_testing_data]) #grayscales images in testing dataset
+
+#end of preprocessing dataset
+
+
+#beginning of training code for CNN model
+
+with tf.Session() as session:
+	session.run(tf.global_variables_initializer())
+	
+	print("Training the model")
+
