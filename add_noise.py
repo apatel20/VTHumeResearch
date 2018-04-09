545d1ca2efe1301191496cb6de3179744a7223f6
apatel20 <apatel20@vt.edu>
2018-04-08 23:24:36 -0400

    code that helps with noise

--- add_noise.py
+++ add_noise.py
@@ -1,5 +1,6 @@
 import numpy as np
 import cv2
+import matplotlib.pyplot as plt
 
 def add_noise(X_image)
 	#create copy as to not modify the actual image
@@ -12,5 +13,22 @@ def add_noise(X_image)
 	#gaussian noise is a statistical method used to generate noise (distribution curve)
 	gaussian_noise = generated_noise.reshape(height,width,channel)
 	image_with_noise = X_image_copy + gaussian_noise
+	imageplot = plt.imshow(image_with_noise)
 	
 return image_with_noise
+
+
+''' if not much noise is generated, this one will work better
+
+from scipy import misc
+
+def add_noise(X_image)
+	X_image_copy = X_image.copy()
+	
+	noise = 2*X_image_copy.max() * np.random.random(X_image_copy.shape)
+	image_with_noise = X_image_copy + noise
+	imageplot = plt.imshow(image_with_noise)
+	
+return image_with noise
+
+'''
